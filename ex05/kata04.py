04074aa577613a9ef5c1fadcdd59cb45cddb675b
Jose Perez Montes <jperez-2@c2r8s6.42malaga.com>
2023-04-21 19:27:26 +0200

    21/04/2023

---
+++ ex05/kata04.py
@@ -0,0 +1,10 @@
+
+'''
+The kata variable is always a tuple that contains, 
+in the following order: 
+• 2 non-negative integer containing up to 2 digits
+• 1 decimal
+• 1 integer
+• 1 decimal
+'''
+kata = (0, 4, 132.42222, 10000, 12345.67)
